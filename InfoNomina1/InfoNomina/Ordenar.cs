e19013c3a5bde0c6b9aa11b2755cb69278ba6150
Jeffry Manobanda <jnmanobanda@hotmail.com>
2024-03-06 02:27:08 -0500

    Actualizacion GUI 1.0

--- InfoNomina1/InfoNomina/Ordenar.cs
+++ InfoNomina1/InfoNomina/Ordenar.cs
@@ -16,7 +16,7 @@ namespace InfoNomina
     public partial class Ordenar : Form
     {
         public static List<string> nombres = new List<string>();
-        public static List<double> sueldos = new List<double>();
+        public static List<float> sueldos = new List<float>();
          
         public Ordenar()
         {
@@ -59,8 +59,9 @@ namespace InfoNomina
 
         private void btnOrdenar_Click(object sender, EventArgs e)
         {
-            nombres.Sort();
-            sueldos.Sort();
+            MergeSortNombres(nombres);
+            MergeSortSueldos(sueldos);
+
             DataTable dataTable = new DataTable();
 
             dataTable.Columns.Add("Apellidos");
@@ -94,7 +95,7 @@ namespace InfoNomina
 
         private void button3_Click(object sender, EventArgs e)
         {
-            nombres.Sort();
+            MergeSortNombres(nombres);
             DataTable dataTable = new DataTable();
 
             dataTable.Columns.Add("Apellidos");
@@ -117,7 +118,7 @@ namespace InfoNomina
 
         private void button4_Click(object sender, EventArgs e)
         {
-            sueldos.Sort();
+            MergeSortSueldos(sueldos);
             DataTable dataTable = new DataTable();
 
             dataTable.Columns.Add("Apellidos");
@@ -136,6 +137,119 @@ namespace InfoNomina
 
             dataGridView1.DataSource = dataTable;
 
+        }
+        private void MergeSortNombres(List<string> nombres)
+        {
+            if (nombres.Count <= 1)
+                return;
+
+            int medio = nombres.Count / 2;
+            List<string> izquierda = new List<string>();
+            List<string> derecha = new List<string>();
+
+            for (int i = 0; i < medio; i++)
+            {
+                izquierda.Add(nombres[i]);
+            }
+
+            for (int i = medio; i < nombres.Count; i++)
+            {
+                derecha.Add(nombres[i]);
+            }
+
+            MergeSortNombres(izquierda);
+            MergeSortNombres(derecha);
+            MergeNombres(nombres, izquierda, derecha);
+        }
+
+        private void MergeNombres(List<string> nombres, List<string> izquierda, List<string> derecha)
+        {
+            int i = 0, j = 0, k = 0;
+            while (i < izquierda.Count && j < derecha.Count)
+            {
+                if (izquierda[i].CompareTo(derecha[j]) < 0)
+                {
+                    nombres[k] = izquierda[i];
+                    i++;
+                }
+                else
+                {
+                    nombres[k] = derecha[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < izquierda.Count)
+            {
+                nombres[k] = izquierda[i];
+                i++;
+                k++;
+            }
+
+            while (j < derecha.Count)
+            {
+                nombres[k] = derecha[j];
+                j++;
+                k++;
+            }
+        }
+
+        private void MergeSortSueldos(List<float> sueldos)
+        {
+            if (sueldos.Count <= 1)
+                return;
+
+            int medio = sueldos.Count / 2;
+            List<float> izquierda = new List<float>();
+            List<float> derecha = new List<float>();
+
+            for (int i = 0; i < medio; i++)
+            {
+                izquierda.Add(sueldos[i]);
+            }
+
+            for (int i = medio; i < sueldos.Count; i++)
+            {
+                derecha.Add(sueldos[i]);
+            }
+
+            MergeSortSueldos(izquierda);
+            MergeSortSueldos(derecha);
+            MergeSueldos(sueldos, izquierda, derecha);
+        }
+
+        private void MergeSueldos(List<float> sueldos, List<float> izquierda, List<float> derecha)
+        {
+            int i = 0, j = 0, k = 0;
+            while (i < izquierda.Count && j < derecha.Count)
+            {
+                if (izquierda[i] < derecha[j])
+                {
+                    sueldos[k] = izquierda[i];
+                    i++;
+                }
+                else
+                {
+                    sueldos[k] = derecha[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < izquierda.Count)
+            {
+                sueldos[k] = izquierda[i];
+                i++;
+                k++;
+            }
+
+            while (j < derecha.Count)
+            {
+                sueldos[k] = derecha[j];
+                j++;
+                k++;
+            }
         }
     }
 }
